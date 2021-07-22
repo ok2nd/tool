9ea432809007ee60db7bc39d2c0a0d4a151b4bb2
ok2nd <m.ok.2nd@gmail.com>
2021-07-22 14:50:34 +0900

    2021.7.22

--- photo/compare/js/compare.js
+++ photo/compare/js/compare.js
@@ -1,30 +1,20 @@
 /*
 	use jquery.ImageExpansion-multi.js
 */
-function compare_image(div1, div2, img1, img2) {
-	var ih_1 = document.getElementById(img1).naturalHeight;
-	var ih_2 = document.getElementById(img2).naturalHeight;
-	var iw_1 = document.getElementById(img1).naturalWidth;
-	var iw_2 = document.getElementById(img2).naturalWidth;
-	if (ih_1 > iw_1) {
-		var pan1 = ih_1 / 500;
-		var pan2 = ih_2 / 500;
-		var drag1 = 180000 / ih_1;
-		var drag2 = 180000 / ih_2;
+function image_expand(div, img) {
+	var ih = document.getElementById(img).naturalHeight;
+	var iw = document.getElementById(img).naturalWidth;
+	if (ih > iw) {
+		var pan = ih / 500;
+		var drag = 180000 / ih;
 	} else {
-		var pan1 = iw_1 / 600;
-		var pan2 = iw_2 / 600;
-		var drag1 = 240000 / iw_1;
-		var drag2 = 240000 / iw_2;
+		var pan = iw / 600;
+		var drag = 240000 / iw;
 	}
 	$(function() {
-		$(div1).ImageExpansion({
-			'pan' : pan1,	//倍率
-			'drag' : drag1	//ドラッグ枠の大きさ
-		});
-		$(div2).ImageExpansion({
-			'pan' : pan2,	//倍率
-			'drag' : drag2	//ドラッグ枠の大きさ
+		$(div).ImageExpansion({
+			'pan' : pan,	//倍率
+			'drag' : drag	//ドラッグ枠の大きさ
 		});
 	});
 }
